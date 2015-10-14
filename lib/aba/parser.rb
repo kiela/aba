f8338ca139f6ea0db120be0017cec64d08f7bdeb
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2015-10-14 16:22:57 +0200

    Allow to parse strings along with all kind of streams
    I did this to allow parsing streams of data provided by other tools that
    File Ruby class. This commit unties gem from using only File Ruby class
    and allows to use gems like Net::SFTP which also provides ability to
    read files line by line. This commit also provides functionality for
    parsing given string which might be read at once using i.e. File.read
    
    Work was done on behalf of Acorns Grow Australia

--- lib/aba/parser.rb
+++ lib/aba/parser.rb
@@ -7,44 +7,56 @@ class Aba
   class Parser
     class Error < ::Exception; end
 
-    attr_reader :filepath, :file, :collection
+    attr_reader :input, :collection
 
-    def initialize(filepath)
-      unless File.exists?(filepath)
-        raise Aba::Parser::Error, "File '#{filepath}' doesn't exist!"
-      end
-
-      @filepath = filepath
-      @file = File.open(@filepath, "r")
+    def initialize(input)
+      @input = input
+      @collection = Array.new
     end
 
     def parse
-      collection || parse!
+      if @input.respond_to?(:gets)
+        parse_stream(@input)
+      elsif @input.is_a?(String)
+        parse_string(@input)
+      else
+        raise Aba::Parser::Error, "Could not parse given input!"
+      end
     end
 
-    def parse!
-      @collection = Array.new
+    def parse_line(line)
+      line = line.gsub("\r", "").gsub("\n", "")
 
-      file.each_line do |line|
-        line.gsub!("\r\n", "")
-        if Aba::Parser::Headers.contains_valid_record_type?(line)
-          @batch = Aba::Batch.new(Aba::Parser::Headers.parse(line))
-        elsif Aba::Parser::Transaction.contains_valid_record_type?(line)
-          @batch.add_transaction(Aba::Parser::Transaction.parse(line))
-        elsif Aba::Parser::Summary.contains_valid_record_type?(line)
-          summary = Aba::Parser::Summary.parse(line)
-          if summary_compatible_with_batch?(summary, @batch)
-            @collection.push(@batch)
-          else
-            raise Aba::Parser::Error, "Summary line for current batch from given doesn't batch calculated summary of that batch!"
-          end
+      if Aba::Parser::Headers.contains_valid_record_type?(line)
+        @batch = Aba::Batch.new(Aba::Parser::Headers.parse(line))
+      elsif Aba::Parser::Transaction.contains_valid_record_type?(line)
+        @batch.add_transaction(Aba::Parser::Transaction.parse(line))
+      elsif Aba::Parser::Summary.contains_valid_record_type?(line)
+        summary = Aba::Parser::Summary.parse(line)
+        if summary_compatible_with_batch?(summary, @batch)
+          @collection.push(@batch)
+        else
+          raise Aba::Parser::Error, "Summary line for current batch from given doesn't batch calculated summary of that batch!"
         end
+      else
+        raise Aba::Parser::Error, "Could not parse given input!"
       end
-
-      @collection
     end
 
     private
+
+      def parse_stream(stream)
+        line = stream.gets
+        until line.nil?
+          parse_line(line)
+          line = stream.gets
+        end
+      end
+
+      def parse_string(string)
+        string = string.split("\n")
+        string.each{ |line| parse_line(line) }
+      end
 
       def summary_compatible_with_batch?(summary, batch)
         summary[:net_total_amount] == batch.net_total_amount &&
@@ -54,20 +66,3 @@ class Aba
       end
   end
 end
-=begin
-aba = Aba.parse(file)
-aba = Aba::Parser.new("file").parse
-#aba.headers
-#aba.summary
-aba.each do |batch|
-  batch.headers
-  batch.each do |transaction|
-  end
-  batch.summary
-end
-
-aba = Aba.batchnew(asd: zxc)
-aba = Aba::Batch(asd: zxc)
-aba.add_transaction({ asd: zxc })
-aba.to_s
-=end
--- spec/lib/aba/parser_spec.rb
+++ spec/lib/aba/parser_spec.rb
@@ -3,76 +3,103 @@
 require "spec_helper"
 
 describe Aba::Parser do
-  let(:filepath) do
-    path = File.join(File.dirname(__FILE__), '../..', 'support/file.aba')
-    File.absolute_path(path)
-  end
-  subject{ described_class.new(filepath) }
+  let(:input) { double }
+  subject{ described_class.new(input) }
 
   describe ".initialize" do
-    context "when file from given path doesn't exist" do
-      it "raises an Aba::Parser::Error with proper message" do
-        allow(File).to receive(:exists?).and_return(false)
-        message = "File '/path/to/non-existent/file' doesn't exist!"
-
-        expect{ described_class.new("/path/to/non-existent/file") }
-          .to raise_error(Aba::Parser::Error, message)
-      end
+    it "sets given input to @input" do
+      expect(subject.instance_variable_get(:@input)).to eq(input)
     end
 
-    context "when file from given path exists" do
-      it "sets filepath to @filepath" do
-        expect(subject.filepath).to eq(filepath)
-      end
-
-      it "opens the file for reading" do
-        expect(File).to receive(:open).with(filepath, "r")
-        described_class.new(filepath)
-      end
-
-      it "sets opened file to @file" do
-        file = instance_double(File)
-        allow(File).to receive(:open).and_return(file)
-
-        expect(subject.file).to eq(file)
-      end
+    it "sets @collection to empty array" do
+      expect(subject.instance_variable_get(:@collection)).to be_empty
     end
   end
 
   describe "#parse" do
-    context "when called for the first time" do
-      it "calls #parse! to proceed" do
-        allow(subject).to receive(:parse!)
+    context "when @input is a stream which responds to .gets" do
+      it "reads input till returns nil" do
+        allow(input).to receive(:respond_to?).with(:gets).and_return(true)
+        allow(input).to receive(:gets).and_return(double, nil)
+        allow(subject).to receive(:parse_line)
 
-        expect(subject).to receive(:parse!)
+        expect(input).to receive(:gets).twice
+        subject.parse
+      end
+
+      it "parses each line received from input" do
+        allow(input).to receive(:respond_to?).with(:gets).and_return(true)
+        line = double
+        allow(input).to receive(:gets).and_return(line, line, nil)
+        allow(subject).to receive(:parse_line)
+
+        expect(subject).to receive(:parse_line).with(line).twice
         subject.parse
       end
     end
 
-    context "when callend for the Nth time" do
-      it "returns memoized collections of batches" do
-        collection = double
-        subject.instance_variable_set(:@collection, collection)
+    context "when @input is a String" do
+      it "splits @input per lines" do
+        allow(input).to receive(:is_a?).with(String).and_return(true)
+        allow(input).to receive(:split).with("\n").and_return(double.as_null_object)
+        allow(subject).to receive(:parse_line)
 
-        expect{ subject.parse }.not_to change{ subject.collection }
-        expect(subject.parse).to eq(collection)
+        expect(input).to receive(:split).with("\n")
+        subject.parse
+      end
+
+      it "parses each line" do
+        allow(input).to receive(:is_a?).with(String).and_return(true)
+        line = double
+        allow(input).to receive(:split).with("\n").and_return([line, line])
+        allow(subject).to receive(:parse_line)
+
+        expect(subject).to receive(:parse_line).with(line).twice
+        subject.parse
+      end
+    end
+
+    context "when @input cannot be parsed" do
+      it "raises an Aba::Parser::Error with proper message" do
+        allow(input).to receive(:respond_to?).with(:gets).and_return(false)
+        allow(input).to receive(:is_a?).with(String).and_return(false)
+        message = "Could not parse given input!"
+
+        expect{ subject.parse }
+          .to raise_error(Aba::Parser::Error, message)
       end
     end
   end
 
-  describe "#parse!" do
-    context "when staring batch line occures" do
+  describe "#parse_line" do
+    it "removes \\r from given line" do
+      line = "0123-345          01WPC       John Doe                  466364Payroll     210915                                        "
+      allow(line).to receive(:gsub).and_return(line)
+
+      expect(line).to receive(:gsub).with("\r", "")
+      subject.parse_line(line)
+    end
+
+    it "removes \\n from given line" do
+      line = "0123-345          01WPC       John Doe                  466364Payroll     210915                                        "
+      allow(line).to receive(:gsub).and_return(line)
+
+      expect(line).to receive(:gsub).with("\n", "")
+      subject.parse_line(line)
+    end
+
+    context "when starting batch line is given" do
       let(:headers_line) { "0123-345          01WPC       John Doe                  466364Payroll     210915                                        " }
 
       before do
-        allow(subject.file).to receive(:each_line).and_yield(headers_line)
+        allow(headers_line).to receive(:gsub).and_return(headers_line)
       end
 
       it "parses the line" do
         allow(Aba::Batch).to receive(:new)
 
         expect(Aba::Parser::Headers).to receive(:parse).with(headers_line)
-        subject.parse!
+        subject.parse_line(headers_line)
       end
 
       it "initializes new batch" do
@@ -81,31 +108,31 @@ describe Aba::Parser do
         allow(Aba::Batch).to receive(:new)
 
         expect(Aba::Batch).to receive(:new).with(parsed_line)
-        subject.parse!
+        subject.parse_line(headers_line)
       end
 
       it "sets new batch to @batch" do
         batch = double
         allow(Aba::Batch).to receive(:new).and_return(batch)
 
-        expect{ subject.parse! }
+        expect{ subject.parse_line(headers_line) }
           .to change{ subject.instance_variable_get(:@batch) }
           .to(batch)
       end
     end
 
-    context "when transaction line occures" do
+    context "when transaction line is given" do
       let(:transaction_line) { "1342-342  3244654 530000010000John Doe                        R435564           453-543 45656733Remitter        00000010" }
       let(:batch) { instance_double(Aba::Batch).as_null_object }
 
       before do
-        allow(subject.file).to receive(:each_line).and_yield(transaction_line)
+        allow(transaction_line).to receive(:gsub).and_return(transaction_line)
         subject.instance_variable_set(:@batch, batch)
       end
 
       it "parses the line" do
         expect(Aba::Parser::Transaction).to receive(:parse).with(transaction_line)
-        subject.parse!
+        subject.parse_line(transaction_line)
       end
 
       it "adds new transaction to current batch" do
@@ -113,7 +140,7 @@ describe Aba::Parser do
         allow(Aba::Parser::Transaction).to receive(:parse).and_return(parsed_line)
 
         expect(batch).to receive(:add_transaction).with(parsed_line)
-        subject.parse!
+        subject.parse_line(transaction_line)
       end
     end
 
@@ -121,14 +148,14 @@ describe Aba::Parser do
       let(:summary_line) { "7999-999            000010000000001000000000000000                        000010                                        " }
 
       before do
-        allow(subject.file).to receive(:each_line).and_yield(summary_line)
+        allow(summary_line).to receive(:gsub).and_return(summary_line)
       end
 
       it "parses the line" do
         allow(subject).to receive(:summary_compatible_with_batch?).and_return(true)
 
         expect(Aba::Parser::Summary).to receive(:parse).with(summary_line)
-        subject.parse!
+        subject.parse_line(summary_line)
       end
 
       context "when summary doesn't match summary of created batch" do
@@ -143,7 +170,8 @@ describe Aba::Parser do
           subject.instance_variable_set(:@batch, batch)
           message = "Summary line for current batch from given doesn't batch calculated summary of that batch!"
 
-          expect{ subject.parse! }.to raise_error(Aba::Parser::Error, message)
+          expect{ subject.parse_line(summary_line) }
+            .to raise_error(Aba::Parser::Error, message)
         end
       end
 
@@ -158,17 +186,11 @@ describe Aba::Parser do
           )
           subject.instance_variable_set(:@batch, batch)
 
-          subject.parse!
+          subject.parse_line(summary_line)
 
           expect(subject.collection).to include(batch)
         end
       end
-    end
-
-    it "returns collections of batches" do
-      allow(subject).to receive(:file).and_return(double.as_null_object)
-
-      expect(subject.parse!).to be_a(Array)
     end
   end
 end
