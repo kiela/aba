4d58dd7bdbbc86aab678ff729114d9b81ec2fc79
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2016-05-09 18:56:03 +0200

    Move headers of a batch to dedicated class

--- lib/aba/batch.rb
+++ lib/aba/batch.rb
@@ -1,41 +1,12 @@
+require "aba/batch/headers"
+
 class Aba
   class Batch
-    include Aba::Validations
-
-    attr_reader :transactions, :credit_total_amount, :debit_total_amount
-    attr_accessor :bsb, :financial_institution, :user_name, :user_id,
-      :description, :process_at
-
-    # BSB
-    validates_bsb         :bsb, allow_blank: true
-
-    # Financial Institution
-    validates_length      :financial_institution, 3
-
-    # User Name
-    validates_presence_of :user_name
-    validates_max_length  :user_name, 26
-    validates_becs        :user_name
-
-    # User ID
-    validates_presence_of :user_id
-    validates_max_length  :user_id, 6
-    validates_integer     :user_id, false
-
-    # Description
-    validates_max_length  :description, 12
-    validates_becs        :description
-
-    # Process at Date
-    validates_length      :process_at, 6
-    validates_integer     :process_at, false
-
+    attr_reader :headers, :transactions, :credit_total_amount,
+      :debit_total_amount
 
     def initialize(attrs = {}, transactions = [])
-      attrs.each do |key, value|
-        send("#{key}=", value)
-      end
-
+      @headers = self.class::Headers.new(attrs)
       @transactions = []
       @credit_total_amount = 0
       @debit_total_amount  = 0
@@ -50,11 +21,12 @@ class Aba
     end
 
     def to_s
-      raise RuntimeError, 'No transactions present - add one using `add_transaction`' if @transactions.empty?
-      raise RuntimeError, 'ABA data is invalid - check the contents of `errors`' unless valid?
+      if @transactions.empty?
+        raise RuntimeError, 'No transactions present - add one using `add_transaction`'
+      end
 
       # Descriptive record
-      output = "#{descriptive_record}\r\n"
+      output = "#{@headers.to_s}\r\n"
 
       # Transactions records
       output += @transactions.map(&:to_s).join("\r\n")
@@ -89,12 +61,12 @@ class Aba
 
     def errors
       # Run validations
-      has_errors?
+      @headers.valid?
       has_transaction_errors?
 
       # Build errors
       all_errors = {}
-      all_errors[:aba] = self.error_collection unless self.error_collection.empty?
+      all_errors[:headers] = @headers.errors unless @headers.errors.empty?
       transaction_error_collection = @transactions.each_with_index.map{ |(k, t), i| [k, t.error_collection] }.reject{ |e| e[1].nil? || e[1].empty? }.to_h
       all_errors[:transactions] = transaction_error_collection unless transaction_error_collection.empty?
 
@@ -113,67 +85,6 @@ class Aba
 
     def has_transaction_errors?
       return @transactions.map(&:valid?).include?(false)
-    end
-
-    def descriptive_record
-      # Record type
-      # Max: 1
-      # Char position: 1
-      output = "0"
-
-      # Optional branch number of the funds account with a hyphen in the 4th character position
-      # Char position: 2-18
-      # Max: 17
-      # Blank filled
-      output += self.bsb.nil? ? " " * 17 : self.bsb.to_s.ljust(17)
-
-      # Sequence number
-      # Char position: 19-20
-      # Max: 2
-      # Zero padded
-      output += "01"
-
-      # Name of user financial instituion
-      # Max: 3
-      # Char position: 21-23
-      output += self.financial_institution.to_s
-
-      # Reserved
-      # Max: 7
-      # Char position: 24-30
-      output += " " * 7
-
-      # Name of User supplying File
-      # Char position: 31-56
-      # Max: 26
-      # Full BECS character set valid
-      # Blank filled
-      output += self.user_name.to_s.ljust(26)
-
-      # Direct Entry User ID
-      # Char position: 57-62
-      # Max: 6
-      # Zero padded
-      output += self.user_id.to_s.rjust(6, "0")
-
-      # Description of payments in the file (e.g. Payroll, Creditors etc.)
-      # Char position: 63-74
-      # Max: 12
-      # Full BECS character set valid
-      # Blank filled
-      output += self.description.to_s.ljust(12)
-
-      # Date on which the payment is to be processed
-      # Char position: 75-80
-      # Max: 6
-      output += self.process_at.to_s.rjust(6, "0")
-
-      # Reserved
-      # Max: 40
-      # Char position: 81-120
-      output += " " * 40
-
-      return output
     end
 
     def batch_control_record
